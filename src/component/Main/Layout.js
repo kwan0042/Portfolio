e94ea6ec7cebbe7fee73408b43f8e9314f926662
kwan0042 <danielkwan6d16@gmail.com>
2024-01-13 11:31:31 -0500

    v22

--- src/component/Main/Layout.js
+++ src/component/Main/Layout.js
@@ -1,32 +1,29 @@
-// import Stars from "./Stars";
-// import Saku from "./Saku";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function Layout() {
-  const [isDark, SetisDark] = useState(false);
-  useEffect(() => {
-    if (localStorage.getItem("theme") === "dark") {
-      SetisDark(true);
-    } else {
-      SetisDark(false);
-    }
-  });
+// export default function Layout() {
+//   const [isDark, SetisDark] = useState(false);
+//   useEffect(() => {
+//     if (localStorage.getItem("theme") === "dark") {
+//       SetisDark(true);
+//     } else {
+//       SetisDark(false);
+//     }
+//   });
 
-  return (
-    <>
-      <div className="mask">
-        <div className="mask-top"></div>
-        <div className="mask-bottom"></div>
-      </div>
+//   return (
+//     <>
+//       <div className="mask">
+//         <div className="mask-top"></div>
+//         <div className="mask-bottom"></div>
+//       </div>
 
-      <div className="frame">
-        <div className="frame_line frame_line-left"></div>
-        <div className="frame_line frame_line-right"></div>
-        <div className="frame_line frame_line-top"></div>
-        <div className="frame_line frame_line-bottom"></div>
-      </div>
+//       <div className="frame">
+//         <div className="frame_line frame_line-left"></div>
+//         <div className="frame_line frame_line-right"></div>
+//         <div className="frame_line frame_line-top"></div>
+//         <div className="frame_line frame_line-bottom"></div>
+//       </div>
 
-      {/* {isDark ? <Stars /> : <Saku />} */}
-    </>
-  );
-}
+//     </>
+//   );
+// }
--- src/component/Main/Saku.js
+++ src/component/Main/Saku.js
@@ -1,17 +1,17 @@
-export default function Saku() {
-  return (
-    <div className="background light">
-      <div className="sakuras">
-        <div className="sakura"></div>
-        <div className="sakura"></div>
-        <div className="sakura"></div>
-        <div className="sakura"></div>
-        <div className="sakura"></div>
-        <div className="sakura"></div>
-        <div className="sakura"></div>
-        <div className="sakura"></div>
-        <div className="sakura"></div>
-      </div>
-    </div>
-  );
-}
+// export default function Saku() {
+//   return (
+//     <div className="background light">
+//       <div className="sakuras">
+//         <div className="sakura"></div>
+//         <div className="sakura"></div>
+//         <div className="sakura"></div>
+//         <div className="sakura"></div>
+//         <div className="sakura"></div>
+//         <div className="sakura"></div>
+//         <div className="sakura"></div>
+//         <div className="sakura"></div>
+//         <div className="sakura"></div>
+//       </div>
+//     </div>
+//   );
+// }
