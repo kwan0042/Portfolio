8a0702e22f9777ad0b1df937b3d17cbe5344ff80
kwan0042 <danielkwan6d16@gmail.com>
2024-09-30 16:33:37 -0400

    v34

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Home from "./component/Home";
+import Start from "./component/Start";
+import Navbar from "./component/Navbar";
+import About from "./component/About";
+import TechLang from "./component/TechLang";
+import Experience from "./component/Experience";
+import Projects from "./component/Projects";
+
+function App() {
+  return (
+    <div className="overflow-x-hidden text-neutral-300 antialiased  selection:text-cy-cyan-900">
+      <Start />
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Home />
+        <About />
+        <TechLang />
+        <Experience />
+        <Projects />
+      </div>
+    </div>
+  );
+}
+
+export default App;
