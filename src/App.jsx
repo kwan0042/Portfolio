59cc28254b290df980be4346a999b29859cf1e65
kwan0042 <danielkwan6d16@gmail.com>
2024-10-15 14:33:37 -0400

    v45.3

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
             <div className="fixed top-0 -z-10 h-full w-full">
               <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
             </div>
-            {/* <Navbar /> */}
+            <Navbar />
             <div className="container mx-auto px-8 mt-[200px]">
               <Routes>
                 <Route
